211c97f1df8b5a4b26f2f84689497ad6a0f84d8f
Westin Miller <equinoxscripts@gmail.com>
2012-02-27 15:09:44 -0800

    Fixed the issue with a bad orthographic projection.

--- 3D Graphics/JOGL Beginner/src/com/pi/gl/graphics/RenderLoop.java
+++ 3D Graphics/JOGL Beginner/src/com/pi/gl/graphics/RenderLoop.java
@@ -2,6 +2,7 @@ package com.pi.gl.graphics;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GL2GL3;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.fixedfunc.GLMatrixFunc;
@@ -25,7 +26,8 @@ public class RenderLoop implements GLEventListener {
 		// Define the world projection
 		gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION);
 		gl.glLoadIdentity();
-		gl.glOrtho(0, drawable.getWidth(), drawable.getHeight(), 0, 1, -1);
+		gl.glOrtho(drawable.getWidth() / -2, drawable.getWidth() / 2,
+				drawable.getHeight() / 2, drawable.getHeight() / -2, -500, 500);
 		gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
 		gl.glLoadIdentity();
 		// This is where the main render logic occurs.
